9936f1840ee8c8eb930b6dc714e01a8f46fe59f6
Sujith Antony <mail2say@gmail.com>
2024-06-18 12:12:59 +0530

    Initial Commit

---
+++ CookingRecipeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookingRecipeApp
+//
+//  Created by Sujith Antony on 18/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookingRecipeApp/CookingRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingRecipeAppApp.swift
+//  CookingRecipeApp
+//
+//  Created by Sujith Antony on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CookingRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
